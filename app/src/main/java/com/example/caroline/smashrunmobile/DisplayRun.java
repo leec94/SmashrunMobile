3aa917f7739be2bb82f5649b5f021ca98f69ff4d
Michael Yu <myu28@brandeis.edu>
2015-04-09 13:37:39 -0400

    fix elevation

--- app/src/main/java/com/example/caroline/smashrunmobile/DisplayRun.java
+++ app/src/main/java/com/example/caroline/smashrunmobile/DisplayRun.java
@@ -29,7 +29,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
-
+    
 
 public class DisplayRun extends ActionBarActivity {
     private JSONObject activityData;
